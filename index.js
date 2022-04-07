7863d5d8568a9920565538756d1e06d7b9eb3db9
elijah <perera.elijah@gmail.com>
2022-04-06 22:10:17 -0700

    started js. incomplete js

---
+++ index.js
@@ -0,0 +1,7 @@
+var btns = document.getElementById(`btn${numArray}`).addEventListener('click', addInputUi);
+
+function addInputUi() {
+    console.log('hi')
+}
+
+var numArray = ["1", "1", "1", "1","1", "1", "1", "1",]
