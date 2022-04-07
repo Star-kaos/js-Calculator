a630c3907ee67385f8af3e66dc011bf95bb23e70
Star_kaos <perera.elijah@gmail.com>
2022-04-07 16:45:08 -0700

    chanched responsiveness and having trouble with eventListeners

--- index.js
+++
@@ -1,7 +0,0 @@
-var btns = document.getElementById(`btn${numArray}`).addEventListener('click', addInputUi);
-
-function addInputUi() {
-    console.log('hi')
-}
-
-var numArray = ["1", "1", "1", "1","1", "1", "1", "1",]
